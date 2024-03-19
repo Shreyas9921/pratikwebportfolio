89bba22e53c8208d46f294bcb14f7c0e73169c3d
Shreyas9921 <55783661+Shreyas9921@users.noreply.github.com>
2024-03-20 00:59:50 +0530

    correction in typos

--- src/components/JScode/data.js
+++ src/components/JScode/data.js
@@ -5,28 +5,28 @@ export const projectData = [
       id: 1,
       title: 'Personal Portfolio Starter',
       description:
-        'A simple static webste built with Next.js & deployed on vercel cloud.',
+        'A simple static website built using Next.js-app & deployed on the Vercel cloud.',
       gitHubLink: 'https://github.com/Shreyas9921/pratik-web-portfolio',
     },
     {
       id: 2,
-      title: 'Books Library App',
+      title: 'Books Library Api',
       description:
-        'A simple Book Library App built with JavaScript. It helps readers have a good list of books they are either currently reading or have finished reading.',
+        'A starter Books library Api built using Spring Boot. It is in the initial development phase.',
       gitHubLink: 'https://github.com/Shreyas9921/book-store-appy',
     },
     {
       id: 3,
       title: 'Low Code, No Code platform sample-design',
       description:
-        'DhiWise, a low code, no code platform based on UX approach, designing applcation with UI/UX perspective',
+        'DhiWise, a low code, no code platform based on UX approach, designing application with UI/UX perspective',
       gitHubLink: 'https://github.com/Shreyas9921/DhiWise-Ecommerce',
     },
     {
       id: 4,
       title: 'FarmFresh prototype',
       description:
-        'E-commerce application, built using plain react and bootstrap with java and spring boot as learning model.',
+        'E-commerce application, built using plain react and bootstrap with java and spring boot as a learning model.',
       gitHubLink: 'https://github.com/CDAC-Project-FarmFresh/farmfresh-project',
     },
     // {
@@ -34,6 +34,6 @@ export const projectData = [
     //   title: 'Twitter UI Clone',
     //   description:
     //     'Simple Twitter UI clone built with TailwindCSS and Vue Js. This covers only the homepage of Twitter UI. This is cool to get started with TailwindCSS as it helps understand basic concepts.',
-    //   gitHubLink: 'https://github.com/olawanlejoel/TwitterUI-clone',
+    //   gitHubLink: '',
     // },
   ];
