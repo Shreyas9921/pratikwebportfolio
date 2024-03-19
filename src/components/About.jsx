fce13165e5331140b5b7c5fc3dfcb1ea111ad05d
Shreyas9921 <55783661+Shreyas9921@users.noreply.github.com>
2024-03-20 00:14:27 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,17 +11,17 @@ const About = () => {
         <div className="about-text">
           <p></p>
           <p>
-            As a CS Engineer, I have always been passionate about creating elegant and effective solutions to
-            uncommon tasks. Adequate knowledge & foundation in software development engineering, with a focus on web
-            technologies such as JavaScript & its flavours as well as Java & its companion framework. 
-            I enjoy both the front-and-back-end technologies to build applications, and I'm always looking for ways to optimize, 
-            improve my newly acquired skills and ensure fair functionality.
+            As a CS Engineer, I've  developed a passion for creating elegant and effective designs & solutions,
+            having Adequate knowledge & foundation in software development, with a focus on web
+            technologies such as JavaScript & its flavors as well as Java & its companion frameworks. 
+            I happen to explore both the UI and logic part on the technologies front to build applications, 
+            and I'm always looking for ways to optimize & improve my newly acquired skills and ensure fair functionality.
           </p>
-          <p>About my career and academics, having recently completed CDAC's PG diploma course in advanced computing,
-            learned array of subjects in software engineering & principles. I'm now equipped with conception to dive in variety 
-            of development tools, platforms and frameworks, including React, Next.js, Node.js, Java, Spring framework, SQL and NoSQL databases
-            SpringToolSuite$4 IDE. I am always eager to learn and explore new technologies on the go, and 
-            I am constantly seeking out opportunities to improve my skills and knowledge.</p>
+          <p>About my career and academics, having recently completed CDAC'S PG diploma course in advanced computing,
+            learned the array of subjects in software engineering & principles. Now equipped with a conception to upskill in  
+            development tools, platforms, and frameworks, including React, Next.js, Node.js, Java, Spring framework, 
+            SQL and NoSQL databases SpringToolSuite$4 IDE to name a few. Always eager to learn and explore new technologies on the go, 
+            constantly seeking out opportunities to improve my skills and knowledge.</p>
         </div>
         <div className="about-img">
           <Image src='/images/about.jpg' className="profile-img" width={300} height={500}/>
