1b56dfd5918db27a5f34e1fc867e03f59e1314f9
Shreyas9921 <55783661+Shreyas9921@users.noreply.github.com>
2024-03-20 10:16:46 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,14 +14,14 @@ const About = () => {
             As a CS Engineer, I've  developed a passion for creating elegant and effective designs & solutions,
             having Adequate knowledge & foundation in software development, with a focus on web
             technologies such as JavaScript & its flavors as well as Java & its companion frameworks. 
-            I happen to explore both the UI and logic part on the technologies front to build applications, 
-            and I'm always looking for ways to optimize & improve my newly acquired skills and ensure fair functionality.
+            I happen to explore both the UX and logic part on the technologies front to build applications, 
+            and I'm always looking for ways to optimize & improve ensuring fair functionality.
           </p>
           <p>About my career and academics, having recently completed CDAC'S PG diploma course in advanced computing,
             learned the array of subjects in software engineering & principles. Now equipped with a conception to upskill in  
-            development tools, platforms, and frameworks, including React, Next.js, Node.js, Java, Spring framework, 
-            SQL and NoSQL databases SpringToolSuite$4 IDE to name a few. Always eager to learn and explore new technologies on the go, 
-            constantly seeking out opportunities to improve my skills and knowledge.</p>
+            development tools, platforms, and frameworks, including React, Next.js, Java, Spring framework, 
+            SQL | NoSQL databases & SpringToolSuite4 to name a few. Always eager to learn and explore new technologies on the go, 
+            constantly seeking opportunities to improve my newly acquired skills and knowledge.</p>
         </div>
         <div className="about-img">
           <Image src='/images/about.jpg' className="profile-img" width={300} height={500}/>
